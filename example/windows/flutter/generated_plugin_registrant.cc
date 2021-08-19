f0f657338c9dce2ef856527b8aec255a5f9bed56
Ebram Tawfik <ebram.s.tawfik@gmail.com>
2021-08-19 16:54:54 -0700

    Windows support

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <presentation_displays/presentation_displays_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  PresentationDisplaysPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PresentationDisplaysPlugin"));
+}
